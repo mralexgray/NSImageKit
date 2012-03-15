b0da8e32e50d6fc013788bcadbbbf7ce805ff9e9
Alex Gray <alex@mrgray.com>
2012-03-15 12:20:45 -0400

    Initial Commit

---
+++ NSImageKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSImageKit
+//
+//  Created by Alex Gray on 3/15/12.
+//  Copyright (c) 2012 mrgray.com, inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ NSImageKit/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  NSImageKit
+//
+//  Created by Alex Gray on 3/15/12.
+//  Copyright (c) 2012 mrgray.com, inc. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
