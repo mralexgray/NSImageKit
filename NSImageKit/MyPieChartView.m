6c1a49d60b86e23a329cd19b6f7348910e565f1c
Alex Gray <alex@mrgray.com>
2012-03-15 17:54:05 -0400

    first commit

---
+++ NSImageKit/MyPieChartView.m
@@ -0,0 +1,474 @@
+//
+//  MyPieChartView.m
+//  CustomNSView
+//
+//  Created by Tim Isted on 27/11/2008.
+//  Copyright Tim Isted © 2008. All rights reserved.
+//
+//  If this is useful to you, please let me know.
+//  All acknowledgements encouraged and gratefully received!
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without
+//  restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
+
+#import "MyPieChartView.h"
+
+
+@implementation MyPieChartView
+
+#pragma mark Initialization and Destruction
+
++ (void)initialize
+{
+	[self exposeBinding:@"segmentNamesArray"];
+	[self exposeBinding:@"segmentValuesArray"];
+	[self exposeBinding:@"selectionIndexes"];
+	[self exposeBinding:@"rotationAmount"];
+}
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+		
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+	[_segmentNamesArray release];
+	[_segmentValuesArray release];
+	
+	if( _segmentPathsArray )
+		[_segmentPathsArray release];
+	
+	if( _segmentTextsArray )
+		[_segmentTextsArray release];
+	
+	[super dealloc];
+}
+
+#pragma mark Drawing
+
+- (void)drawRect:(NSRect)rect
+{
+	[[NSColor whiteColor] set]; // white background
+	NSRectFill([self bounds]);
+	
+	if( [self inLiveResize] )
+	{
+		[self generateDrawingInformation];
+	}
+	
+	NSArray *pathsArray = [self segmentPathsArray];
+	unsigned count;
+	for( count = 0; count < [pathsArray count]; count++ )
+	{
+		NSBezierPath *eachPath = [pathsArray objectAtIndex:count];
+		
+		// fill the path with the drawing color for this index unless it's selected
+		if( [[self selectionIndexes] containsIndex:count] )
+			[[NSColor blueColor] set];
+		else
+			[[self colorForIndex:count] set];
+		
+		[eachPath fill];
+		
+		// draw a black border around it
+		[[NSColor blackColor] set];
+		[eachPath stroke];
+	}
+	
+	NSArray *textsArray = [self segmentTextsArray];
+	
+	for( count = 0; count < [textsArray count]; count++ )
+	{
+		NSDictionary *eachTextDictionary = [textsArray objectAtIndex:count];
+		NSPoint textPoint = NSMakePoint( [[eachTextDictionary valueForKey:@"textPointX"] floatValue], [[eachTextDictionary valueForKey:@"textPointY"] floatValue] );
+		
+		NSDictionary *textAttributes = [eachTextDictionary valueForKey:@"textAttributes"];
+		
+		NSString *text = [eachTextDictionary valueForKey:@"text"];
+		[text drawAtPoint:textPoint withAttributes:textAttributes];
+	}
+}
+
+- (NSColor *)randomColor
+{
+	float red = (random()%1000)/1000.0;
+	float green = (random()%1000)/1000.0;
+	float blue = (random()%1000)/1000.0;
+	float alpha = (random()%1000)/1000.0;
+	return [NSColor colorWithCalibratedRed:red green:green blue:blue alpha:alpha];
+}
+
+- (NSColor *)colorForIndex:(unsigned)index
+{
+	static NSMutableArray *colorsArray = nil;
+	
+	if( colorsArray == nil )
+	{
+		colorsArray = [[NSMutableArray alloc] init];
+	}
+	
+	if( index >= [colorsArray count] )
+	{
+		unsigned currentNum = 0;
+		for( currentNum = [colorsArray count]; currentNum <= index; currentNum++ )
+		{
+			[colorsArray addObject:[self randomColor]];
+		}
+	}
+	
+	return [colorsArray objectAtIndex:index];
+}
+
+- (void)generateDrawingInformation
+{
+	// Keep pointers to the segmentValuesArray and segmentNamesArray
+	NSArray *cachedSegmentValuesArray = [self segmentValuesArray];
+	NSArray *cachedSegmentNamesArray = [self segmentNamesArray];
+	
+	// Get rid of any existing Paths Array
+	if( _segmentPathsArray )
+	{
+		[_segmentPathsArray release];
+		_segmentPathsArray = nil;
+	}
+	
+	// Get rid of any existing Texts Array
+	if( _segmentTextsArray )
+	{
+		[_segmentTextsArray release];
+		_segmentTextsArray = nil;
+	}
+	
+	// If there aren't any values to display, we can exit now
+	if( [cachedSegmentValuesArray count] < 1 )
+		return;
+	
+	// Get the sum of the amounts and exit if it is zero
+	float sumOfAmounts = 0;
+	for( NSNumber *eachAmountToSum in cachedSegmentValuesArray )
+		sumOfAmounts += [eachAmountToSum floatValue];
+	
+	if( sumOfAmounts == 0 )
+		return;
+	
+	_segmentPathsArray = [[NSMutableArray alloc] initWithCapacity:[cachedSegmentValuesArray count]];
+	_segmentTextsArray = [[NSMutableArray alloc] initWithCapacity:[cachedSegmentValuesArray count]];
+	
+	NSIndexSet *selectionIndexes = [self selectionIndexes];
+	BOOL shouldOffsetSelectedSegment = ([selectionIndexes count] > 0) ? YES : NO;
+	
+#define PADDINGAROUNDGRAPH 20.0
+#define TEXTPADDING 5.0
+#define SELECTEDSEGMENTOFFSET 5.0
+	
+	NSRect viewBounds = [self bounds];
+	NSRect graphRect = NSInsetRect(viewBounds, PADDINGAROUNDGRAPH, PADDINGAROUNDGRAPH);
+	
+	// Make the graphRect square and centred
+	if( graphRect.size.width > graphRect.size.height )
+	{
+		double sizeDifference = graphRect.size.width - graphRect.size.height;
+		graphRect.size.width = graphRect.size.height;
+		graphRect.origin.x += (sizeDifference / 2);
+	}
+	
+	if( graphRect.size.height > graphRect.size.width )
+	{
+		double sizeDifference = graphRect.size.height - graphRect.size.width;
+		graphRect.size.height = graphRect.size.width;
+		graphRect.origin.y += (sizeDifference / 2);
+	}
+	
+	// get NSRects for the different quarters of the pie-chart
+	NSRect topLeftRect, topRightRect;
+	NSDivideRect(viewBounds, &topLeftRect, &topRightRect, (viewBounds.size.width / 2), NSMinXEdge );
+	NSRect bottomLeftRect, bottomRightRect;
+	NSDivideRect(topLeftRect, &topLeftRect, &bottomLeftRect, (viewBounds.size.height / 2), NSMinYEdge );
+	NSDivideRect(topRightRect, &topRightRect, &bottomRightRect, (viewBounds.size.height / 2), NSMinYEdge );
+	
+	// Calculate how big a 'unit' is
+	float unitSize = (360.0 / sumOfAmounts);
+	
+	if( unitSize > 360 )
+		unitSize = 360;
+	
+	float radius = graphRect.size.width / 2;
+	
+	NSPoint midPoint = NSMakePoint( NSMidX(graphRect), NSMidY(graphRect) );
+	
+	// Set the text attributes to be used for each textual display
+	NSDictionary *textAttributes = [NSDictionary dictionaryWithObjectsAndKeys: [NSColor whiteColor], NSBackgroundColorAttributeName, [NSColor blackColor], NSForegroundColorAttributeName, [NSFont systemFontOfSize:12], NSFontAttributeName, nil];
+	
+	// cycle through the segmentValues and create the bezier paths
+	// Also add the text details (note we expect the texts' indexes to tie up with the values' indexes)
+	float currentDegree = [[self rotationAmount] floatValue];
+	unsigned currentIndex;
+	for( currentIndex = 0; currentIndex < [cachedSegmentValuesArray count]; currentIndex++ )
+	{
+		NSNumber *eachValue = [cachedSegmentValuesArray objectAtIndex:currentIndex];
+		
+		float startDegree = currentDegree;
+		currentDegree += ([eachValue floatValue] * unitSize);
+		float endDegree = currentDegree;
+		float midDegree = startDegree + ((endDegree - startDegree) / 2);
+		
+		NSBezierPath *eachSegmentPath = [NSBezierPath bezierPath];
+		[eachSegmentPath moveToPoint:midPoint];
+		
+		[eachSegmentPath appendBezierPathWithArcWithCenter:midPoint radius:radius startAngle:startDegree endAngle:midDegree clockwise:NO];
+		
+		NSPoint textPoint = [eachSegmentPath currentPoint];
+		
+		[eachSegmentPath appendBezierPathWithArcWithCenter:midPoint radius:radius startAngle:midDegree endAngle:endDegree clockwise:NO];
+		
+		[eachSegmentPath closePath]; // close path also handles the lines from the midPoint to the start and end of the arc
+		[eachSegmentPath setLineWidth:2.0];
+		
+		// Check to see whether we should offset this segment if it's currently selected in the array controller
+		if( shouldOffsetSelectedSegment && [selectionIndexes containsIndex:currentIndex] )
+		{
+			float differenceRatio = (SELECTEDSEGMENTOFFSET / radius) + (SELECTEDSEGMENTOFFSET / (endDegree - startDegree));
+			
+			float diffY = (textPoint.y - midPoint.y) * differenceRatio;
+			float diffX = (textPoint.x - midPoint.x) * differenceRatio;
+			
+			NSAffineTransform *transform = [NSAffineTransform transform];
+			
+			[transform translateXBy:diffX yBy:diffY];
+			[eachSegmentPath transformUsingAffineTransform: transform];
+			
+			textPoint = [transform transformPoint:textPoint];
+		}
+		
+		[_segmentPathsArray addObject:eachSegmentPath];
+		
+		// Get the text to be displayed, if it exists, and see how big it is
+		NSString *eachText = @"";
+		if( [cachedSegmentNamesArray count] > currentIndex )
+			eachText = [cachedSegmentNamesArray objectAtIndex:currentIndex];
+		
+		NSSize textSize = [eachText sizeWithAttributes:textAttributes];
+		
+		// Offset it by TEXTPADDING in direction suitable for whichever quarter of the view it is in
+		if( NSPointInRect(textPoint, topLeftRect) )
+		{
+			textPoint.y -= (textSize.height + TEXTPADDING);
+			textPoint.x -= (textSize.width + TEXTPADDING);
+		}
+		else if( NSPointInRect(textPoint, topRightRect) )
+		{
+			textPoint.y -= (textSize.height + TEXTPADDING);
+			textPoint.x += TEXTPADDING;
+		}
+		else if( NSPointInRect(textPoint, bottomLeftRect) )
+		{
+			textPoint.y += TEXTPADDING;
+			textPoint.x -= (textSize.width + TEXTPADDING);
+		}
+		else if( NSPointInRect(textPoint, bottomRightRect) )
+		{
+			textPoint.y += TEXTPADDING;
+			textPoint.x += TEXTPADDING;
+		}
+		
+		// Make sure the point isn't outside the view's bounds
+		if( textPoint.x < viewBounds.origin.x )
+			textPoint.x = viewBounds.origin.x;
+		
+		if( (textPoint.x + textSize.width) > (viewBounds.origin.x + viewBounds.size.width) )
+			textPoint.x = viewBounds.origin.x + viewBounds.size.width - textSize.width;
+		
+		if( textPoint.y < viewBounds.origin.y )
+			textPoint.y = viewBounds.origin.y;
+		
+		if( (textPoint.y + textSize.height) > (viewBounds.origin.y + viewBounds.size.height) )
+			textPoint.y = viewBounds.origin.y + viewBounds.size.height - textSize.height;
+		
+		// Finally add the details as a dictionary to our segmentTextsArray.
+		// We include here the textAttributes lest we decide later to e.g. color the texts the same color as the segment fill
+		[_segmentTextsArray addObject:[NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithFloat:textPoint.x], @"textPointX", [NSNumber numberWithFloat:textPoint.y], @"textPointY", eachText, @"text", textAttributes, @"textAttributes", nil]];
+	}
+}
+
+#pragma mark Event Handling
+- (int)objectIndexForPoint:(NSPoint)thePoint
+{
+	NSArray *cachedPathsArray = [self segmentPathsArray];
+	
+	int count;
+	for( count = 0; count < [cachedPathsArray count]; count++ )
+	{
+		NSBezierPath *eachPath = [cachedPathsArray objectAtIndex:count];
+		
+		if( [eachPath containsPoint:thePoint ] )
+		{
+			return count;
+		}
+	}
+	
+	// if control reaches here, no segment contained the point so return -1	
+	return -1;
+}
+
+- (void)mouseUp:(NSEvent *)theEvent
+{
+	int index = [self objectIndexForPoint:[self convertPoint:[theEvent locationInWindow] fromView:nil]];
+	
+	NSMutableIndexSet *newSelectionIndexes = [[self selectionIndexes] mutableCopy];
+	
+	if ( [theEvent modifierFlags] & NSCommandKeyMask )
+	{
+		// Add or remove the clicked segment
+		if ( [newSelectionIndexes containsIndex:index] )
+		{
+			[newSelectionIndexes removeIndex:index];
+		}
+		else
+		{
+			[newSelectionIndexes addIndex:index];
+		}
+	}
+	else if ( [theEvent modifierFlags] & NSShiftKeyMask )
+	{
+		// Add range to selection
+		if ([newSelectionIndexes count] == 0)
+		{
+			[newSelectionIndexes addIndex:index];
+		}
+		else
+		{
+			unsigned int origin = (index < [newSelectionIndexes lastIndex]) ? index :[newSelectionIndexes lastIndex];
+			unsigned int length = (index < [newSelectionIndexes lastIndex]) ? [newSelectionIndexes lastIndex] - index : index - [newSelectionIndexes lastIndex];
+			
+			length++;
+			[newSelectionIndexes addIndexesInRange:NSMakeRange(origin, length)];
+		}
+	}
+	else // the user just clicked without modifier keys so simply select the segment
+	{
+		[newSelectionIndexes removeAllIndexes];
+		if( index >= 0 )
+			[newSelectionIndexes addIndex:index];
+	}
+	
+	[self setSelectionIndexes:newSelectionIndexes];
+	[newSelectionIndexes release];
+}
+
+#pragma mark View Behavior Overrides
+
+- (BOOL)isFlipped
+{
+	return YES;
+}
+
+- (BOOL)acceptsFirstResponder
+{
+	return YES;
+}
+
+#pragma mark Accessors
+
+- (NSArray *)segmentNamesArray
+{
+	return [[_segmentNamesArray retain] autorelease];
+}
+
+- (void)setSegmentNamesArray:(NSArray *)newArray
+{
+	[self willChangeValueForKey:@"segmentNamesArray"];
+	[_segmentNamesArray release];
+	_segmentNamesArray = [newArray copy];
+	[self didChangeValueForKey:@"segmentNamesArray"];
+	
+	[self generateDrawingInformation];
+	[self setNeedsDisplayInRect:[self visibleRect]];
+}
+
+- (NSArray *)segmentValuesArray
+{
+	return [[_segmentValuesArray retain] autorelease];
+}
+
+- (void)setSegmentValuesArray:(NSArray *)newArray
+{
+	[self willChangeValueForKey:@"segmentValuesArray"];
+	[_segmentValuesArray release];
+	_segmentValuesArray = [newArray copy];
+	[self didChangeValueForKey:@"segmentValuesArray"];
+	
+	[self generateDrawingInformation];
+	[self setNeedsDisplayInRect:[self visibleRect]];
+}
+
+- (NSArray *)segmentPathsArray
+{
+	return _segmentPathsArray;
+}
+
+- (NSArray *)segmentTextsArray
+{
+	return _segmentTextsArray;
+}
+
+- (NSIndexSet *)selectionIndexes
+{
+    return [[_selectionIndexes retain] autorelease]; 
+}
+
+- (void)setSelectionIndexes:(NSIndexSet *)newIndexes
+{
+	if ((_selectionIndexes != newIndexes) && (![_selectionIndexes isEqualToIndexSet:newIndexes]))
+	{
+		[self willChangeValueForKey:@"selectionIndexes"];
+		[_selectionIndexes release];
+		_selectionIndexes = [newIndexes copy];
+		[self didChangeValueForKey:@"selectionIndexes"];
+		
+		[self generateDrawingInformation];
+		[self setNeedsDisplayInRect:[self visibleRect]];
+	}
+}
+
+- (NSNumber *)rotationAmount
+{
+	return [NSNumber numberWithFloat:_rotationAmount];
+}
+
+- (void)setRotationAmount:(NSNumber *)value
+{
+	if( [value floatValue] != _rotationAmount )
+	{
+		[self willChangeValueForKey:@"rotationAmount"];
+		_rotationAmount = [value floatValue];
+		[self didChangeValueForKey:@"rotationAmount"];
+		
+		[self generateDrawingInformation];
+		[self setNeedsDisplayInRect:[self visibleRect]];
+	}
+}
+
+@end
---
+++ colors.base64.php
@@ -0,0 +1,185 @@
+<?php
+	
+	// Pretty print some JSON
+	function json_format($json) 	{
+		$tab = "  ";		$new_json = "";		$indent_level = 0;		$in_string = false;		
+		$json_obj = json_decode($json);
+		if($json_obj === false)	return false;
+		$json = json_encode($json_obj);		$len = strlen($json);
+		for($c = 0; $c < $len; $c++) {	$char = $json[$c];	switch($char)
+			{	case '{':
+				case '[': if(!$in_string) {	$new_json .= $char . "\n" . str_repeat($tab, $indent_level+1);	$indent_level++; }
+					else { 	$new_json .= $char;	} break;
+				case '}':
+				case ']': if(!$in_string) {	$indent_level--; $new_json .= "\n" . str_repeat($tab, $indent_level) . $char; }
+					else {	$new_json .= $char; } break;
+				case ',': if(!$in_string) { $new_json .= ",\n" . str_repeat($tab, $indent_level); }
+					else { $new_json .= $char; }
+					break;
+				case ':': if(!$in_string) { $new_json .= ": ";	}
+					else { $new_json .= $char; 	}
+					break;
+				case '"': if($c > 0 && $json[$c-1] != '\\') { $in_string = !$in_string; }
+				default: $new_json .= $char; break;                   
+			}
+		}
+		return $new_json;
+	}
+	
+	
+	/** This class can be used to get the most common colors in an image. It needs 1 param: $image, which is the filename of the image you want to process. */
+	class GetMostCommonColors	{
+		/*** The filename of the image (it can  a JPG, GIF or PNG image)   ** @var string*/
+		var $base64image;
+		var $image; 
+		/** Returns the colors of the image in array, in descending order, keys -> thcolors, values -> count of the color. @return array */
+		function Get_Color() {
+			
+			$im = imagecreatefromstring($this->image);
+			//			if (isset($this->image)) { $PREVIEW_WIDTH = 30; // RESIZE THE IMAGE, WE ONLY NEED THE MOST SIG COLORS.	$PREVIEW_HEIGHT   = 30;
+			// $size = GetImageSize($dataImage);  //$this->image);
+			//	$scale=1; if ($size[0]>0) $scale = min($PREVIEW_WIDTH/$size[0], $PREVIEW_HEIGHT/$size[1]);
+			//	if ($scale < 1)	{ $width = floor($scale*$size[0]); $height = floor($scale*$size[1]); } else {	
+			// $width = $size[0];		$height = $size[1];		//	}
+			// $image_resized = imagecreatetruecolor($width, $height);
+			// if ($size[2]==1) $image_orig=imagecreatefromgif($this->image);
+			// if ($size[2]==2) $image_orig=imagecreatefromjpeg($this->image);
+			// if ($size[2]==3) $image_orig=imagecreatefrompng($this->image);
+			//WE NEED NEAREST NEIGHBOR RESIZING, BECAUSE IT DOESN'T ALTER THE COLORS
+			// imagecopyresampled($image_resized, $image_orig, 0, 0, 0, 0, $width, $height, $size[0], $size[1]);
+			// $im = $image_resized;
+			// $im = $image_resized;
+			
+			$imgWidth = imagesx($im);			$imgHeight = imagesy($im);
+			for ($y=0; $y < $imgHeight; $y++) {
+				for ($x=0; $x < $imgWidth; $x++) {
+					$index = imagecolorat($im,$x,$y);		$Colors = imagecolorsforindex($im,$index);
+					//ROUND THE COLORS, TO REDUCE THE NUMBER OF COLORS, SO THE WON'T BE ANY NEARLY DUPLICATE COLORS!
+					$Colors['red']=intval((($Colors['red'])+15)/32)*32;
+					$Colors['green']=intval((($Colors['green'])+15)/32)*32;
+					$Colors['blue']=intval((($Colors['blue'])+15)/32)*32;
+					if ($Colors['red']>=256) $Colors['red']=240; if ($Colors['green']>=256) $Colors['green']=240; if ($Colors['blue']>=256) $Colors['blue']=240;
+					$hexarray[]=substr("0".dechex($Colors['red']),-2).substr("0".dechex($Colors['green']),-2).substr("0".dechex($Colors['blue']),-2);
+				}	
+			}	$hexarray=array_count_values($hexarray);   natsort($hexarray);  	$hexarray=array_reverse($hexarray,true);	return $hexarray;
+		}
+	}
+	
+	
+	$X=$argv[1]; 
+	echo "arg 1 = $X";	
+	$imagedata = base64_decode($X);
+	$im = imagecreatefromstring($imagedata);
+	imagealphablending($im, true); // setting alpha blending on
+	imagesavealpha($im, true); // save alphablending setting (important)
+	$imgWidth = imagesx($im);			$imgHeight = imagesy($im);
+	for ($y=0; $y < $imgHeight; $y++) {
+		for ($x=0; $x < $imgWidth; $x++) {
+			$index = imagecolorat($im,$x,$y);		$Colors = imagecolorsforindex($im,$index);
+			//ROUND THE COLORS, TO REDUCE THE NUMBER OF COLORS, SO THE WON'T BE ANY NEARLY DUPLICATE COLORS!
+			$Colors['red']=intval((($Colors['red'])+15)/32)*32;
+			$Colors['green']=intval((($Colors['green'])+15)/32)*32;
+			$Colors['blue']=intval((($Colors['blue'])+15)/32)*32;
+			if ($Colors['red']>=256) $Colors['red']=240; if ($Colors['green']>=256) $Colors['green']=240; if ($Colors['blue']>=256) $Colors['blue']=240;
+			$hexarray[]=substr("0".dechex($Colors['red']),-2).substr("0".dechex($Colors['green']),-2).substr("0".dechex($Colors['blue']),-2);
+	}	}	$hexarray=array_count_values($hexarray);   natsort($hexarray);  	$hexarray=array_reverse($hexarray,true);	
+	print_r(json_encode($hexarray));
+
+/* 	$username = get_current_user();
+	$File = "/Users/localadmin/desktop/test.png"; 
+	imagepng($im, $File); 
+*/
+
+
+	// $commonColors=new GetMostCommonColors();
+	// $commonColors->image="$PHPIMAGE";
+	// $colors=$commonColors->Get_Color();
+	// $swatches =  array();
+	// $colors_key=array_keys($colors);
+	// for ($i = 1; $i <= 6; $i++) $swatches[] = "#".$colors_key[$i];
+	// print_r($swatches);
+	
+	// $value["swatches"] = $swatches;
+	// 	//		array_push($apps, $app);
+	// }
+	// 
+	// 
+	// $r = array();
+	// $r["apps"]= $var5;
+
+	// $username = get_current_user();
+	// $File = $argv[2]."/appsWithColors.json"; 
+	// $Handle = fopen($File, 'w');
+	// //	$Data = $var5; 	
+	// fwrite($Handle, json_format(json_encode($r))); 
+	// print "Data Written to $File"; 
+	// fclose($Handle); 
+
+	// $var5 = json_decode ($X, true);	
+	// foreach($var5 as &$value ) {   //	echo $value->app;
+		
+		// $PHPIMAGE = $value['proxyIconPath'];
+
+		// $ex->image="$PHPIMAGE";
+		//		$app = array(); 	
+		//		$app[$value->app] = $value->result_array();
+	
+	
+	//	$swatches = array();
+	//	$swatches= array();
+	//	$swatches['swatches'], $colors_key);
+	//	array_push($swatches, $value);	
+	
+	//	$colors_key=array_keys($colors);
+	//	for ($i = 1; $i <= $how_many; $i++) {
+	//		$it = $colors_key[$i];  //		echo $it ;
+	//		$colornow='color'.$i;
+	//		$value->$colornow = $it;
+	
+	//	}
+	
+	
+	
+	//	nameColor($it);
+	//	$islast = ($i);
+	//	if ($islast < $how_many) echo ";";
+	//	$PHPIMAGE=$X;
+	//	$apps = json_decode($Y);
+	//$pattern = array(',"', '{', '}');
+	//$replacement = array(",\n\t\"", "{\n\t", "\n}");
+	//str_replace($pattern, $replacement, 
+	
+	//$json = json_encode($var5);
+	//$prettyJson = $uglyjson;
+	//indent($uglyjson);
+	//$jsonOut = ;
+	
+	//print_r($var5);
+	
+	
+	//print_r("i got htis from YOU occoa...", $apps);
+	
+	//$ex=new GetMostCommonColors();
+	//$ex->image="$PHPIMAGE";
+	//$colors=$ex->Get_Color();
+	//$how_many=5;
+	//$colors_key=array_keys($colors);
+	//
+	//for ($i = 1; $i <= $how_many; $i++) {
+	//	$it = $colors_key[$i];
+	//	echo $it ;
+	//		//	nameColor($it);
+	//	$islast = ($i);
+	//	if ($islast < $how_many) echo ";";
+	//	echo ":";
+	//."," .$colors[$colors_key[$i]]. "</div>";
+	
+	// echo "<div class='swatch' style=' background-color: #".$colors_key[$i].";'>" .$colors[$colors_key[$i]]. "</div>";
+	//}
+	//$x=2222;        					//only for creating the sample object  //creating second subarray for objects of selected class  in another way
+	//$arr[get_class(new Org2("wtf".$x))] = array();
+	//for ($i=0; $i<3; $i++) {  	//pushing some new objects to (sub)arrays in (main)array
+	//	$org1 = new Org("tst".$i);	//	$org2 = new Org2("wtf".$i); 	//	array_push($arr[get_class($org1)], $org1);
+	//	array_push($arr[get_class($org2)], $org2);
+	//}
+?>	
